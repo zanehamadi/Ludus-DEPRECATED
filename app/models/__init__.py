98da07cdb71240d05f8c57355e331f9a0fe67fc6
zaneh <zanehamadi@gmail.com>
2021-09-21 21:26:11 -0400

    finalized Game,WantToPlay table additions

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,4 @@
 from .db import db
 from .user import User
+from .game import Game
+from .wantToPlay import WantToPlay
--- app/models/game.py
+++ app/models/game.py
@@ -1,6 +1,7 @@
 from .db import db
 
 class Game(db.Model):
+    
     __tablename__ = 'games'
 
     id = db.Column(db.Integer, primary_key=True)
