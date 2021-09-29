ceaf76b2241895054cb9b6a6b76cfcc966f61d54
zaneh <zanehamadi@gmail.com>
2021-09-29 17:26:04 -0400

    added password validator

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
+import re
 
 
 def user_exists(form, field):
@@ -17,9 +18,17 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+def password_validation(form, field):
+    
+    password = field.data
+    regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+    matches = re.fullmatch(regex, password)
+    if not matches and len(password):
+        raise ValidationError("Password must have atleast eight characters, one lowercase, one uppercase, one number, and one special character.")
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists, Email()])
-    password = StringField('password', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired(), password_validation])
+
